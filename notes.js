c33031e5bcb1fc10bbdb44b76ec1f1d74aa8582b
Jeff <none>
2018-11-02 18:01:48 +0000

    simplified input with Yargs

--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log('Starting notes.js')
 
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New Note';
+var addNote = (title, body) => {
+	console.log('Adding note', title, body);
 };
+
+var getAll = () => {
+	console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+	console.log('Read the note with title', title );
+};
+
+var removeNote = (title) => {
+	console.log('Removed note with title', title);
+};
+
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	removeNote
+}
