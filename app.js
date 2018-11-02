9964ba3a18d70d41e413ce7d569699852491b580
Jeff <none>
2018-11-02 16:55:59 +0000

    using 3rd party modules

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
+//console.log(_.isString(true));
+//console.log(_.isString('Andrew'));
+var filteredArray = _.uniq(['Andrew',1,'Andrew',1,2,3,4]);
+console.log(filteredArray)
 
-console.log('Result:',notes.add(1,2));
+//console.log('Result:',notes.add(1,2));
 
 //var user = os.userInfo();
 //fs.appendFileSync('greetings.txt',`Hello ${user.username}! Your are ${notes.age}.`);
+
