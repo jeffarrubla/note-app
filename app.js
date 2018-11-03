95e8b1340e4649aefb424dcc1a16085303a6d4e7
Jeff <none>
2018-11-03 20:05:04 +0000

    Requiring arguments and advance yargs

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions =  {
+			describe: 'Title of note',
+			demand: true,
+			alias: 't'
+		};
+const bodyOptions =  {
+			describe: 'body of note',
+			demand: true,
+			alias: 'b'
+		};
+const argv = yargs
+	.command('add','Add a new note',{
+		title: titleOptions,
+		body: bodyOptions
+	})
+	.command('list','List all notes')
+	.command('read','Read all notes',{
+		title: titleOptions
+	}).command('remove','Remove a note',{
+		title: titleOptions
+	})
+	.help() //add help to see on shell
+	.argv;
 var command = argv._[0];
 
 if(command === 'add') {
